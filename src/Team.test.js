abec280d49234044404bd117ac3c456bccf459aa
Jesse Q. Oremland <joremland@vcu.edu>
2018-05-01 19:33:18 -0400

    Commit build

--- src/Team.test.js
+++ src/Team.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Team from './Team';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Team />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
